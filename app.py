a6ab3c95dc4458e1375aa84960abf2d1c7884538
bsWiwat <bs.wiwat@gmail.com>
2025-03-14 22:16:46 +0700

    update(): fix req for deploy.

--- app.py
+++ app.py
@@ -168,5 +168,5 @@ def predict_fashion():
 
 if __name__ == '__main__':
     # Run the Flask app
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
